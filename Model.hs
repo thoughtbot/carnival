03b5d2ddb628769bccf97733008c1f0bcc372bc8
patrick brisbin <pbrisbin@gmail.com>
2014-02-07 10:38:04 -0500

    Remove comment

--- Model.hs
+++ Model.hs
@@ -7,10 +7,6 @@ import Database.Persist.Quasi
 import Control.Applicative
 import Control.Monad
 
--- You can define all of your database entities in the entities file.
--- You can find more information on persistent and how to declare entities
--- at:
--- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
 
