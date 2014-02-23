5470fbe28ed7606a17f3220594123cf2f265a383
patrick brisbin <pbrisbin@gmail.com>
2014-02-23 12:55:19 -0500

    Use separate template file, .container by default

--- Handler/Session.hs
+++ Handler/Session.hs
@@ -10,20 +10,4 @@ getSessionR = do
 
     defaultLayout $ do
         setTitle "Session Info"
-
-        [whamlet|
-            <div .container>
-                <h1>Session Info
-
-                <h3>Profile:
-
-                $maybe Entity _ u <- mu
-                    <ul>
-                        <li><strong>Name</strong>: #{userFirstName u} #{userLastName u}
-                        <li><strong>Email</strong>: #{userEmail u}
-                $nothing
-                    <p>Not authenticated
-
-                <h3>Session:
-                <pre>#{show sess}
-        |]
+        $(widgetFile "session")
