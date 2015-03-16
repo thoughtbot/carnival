0e697eae9fb7c35d5c7901ed0e18fe13c08e60f7
patrick brisbin <pbrisbin@gmail.com>
2015-03-16 15:11:37 -0400

    Add highlight.js
    - Comment opacity rule in highlight.css
    - Use <pre><code> in embed-example

--- Foundation.hs
+++ Foundation.hs
@@ -62,7 +62,9 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
+            addScript $ StaticR js_highlight_js
             addStylesheet $ StaticR css_bootstrap_css
+            addStylesheet $ StaticR css_highlight_css
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
