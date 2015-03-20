237e868622598fd4684f84f1ffbedc3820b159fa
patrick brisbin <pbrisbin@gmail.com>
2015-03-20 14:31:11 -0400

    s/PlanName/Plan/

--- Plan.hs
+++ Plan.hs
@@ -1,5 +1,5 @@
 module Plan
-    ( PlanName(..)
+    ( Plan(..)
     , plans
     , planFeatures
     , planSiteCount
@@ -13,28 +13,28 @@ import Settings
 import Database.Persist.TH (derivePersistField)
 import Yesod.Core
 
-data PlanName
+data Plan
     = Personal
     | Business
     | Enterprise
     deriving (Bounded, Enum, Eq, Read, Show)
 
-$(derivePersistField "PlanName")
+$(derivePersistField "Plan")
 
-plans :: [PlanName]
+plans :: [Plan]
 plans = [minBound .. maxBound]
 
-planFeatures :: PlanName -> WidgetT m IO ()
+planFeatures :: Plan -> WidgetT m IO ()
 planFeatures Personal = $(widgetFile "plans/personal")
 planFeatures Business = $(widgetFile "plans/business")
 planFeatures Enterprise = $(widgetFile "plans/enterprise")
 
-planSiteCount :: PlanName -> Int
+planSiteCount :: Plan -> Int
 planSiteCount Personal = 1
 planSiteCount Business = 5
 planSiteCount Enterprise = 10000
 
-planCommentCount :: PlanName -> Int
+planCommentCount :: Plan -> Int
 planCommentCount Personal = 10
 planCommentCount Business = 100
 planCommentCount Enterprise = 100000
